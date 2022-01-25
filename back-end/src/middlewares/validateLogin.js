b152e06df829b54a32e7365ce2884b5e2f58c37e
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-25 20:41:09 -0300

    Move schema de login para pasta schema

--- back-end/src/middlewares/validateLogin.js
+++ back-end/src/middlewares/validateLogin.js
@@ -1,23 +1,11 @@
-const Joi = require('joi');
-
-const schema = Joi.object(
-  {
-    name: Joi.string().min(12).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-    role: Joi.string(),
-  },
-);
+const { login } = require('../schemas');
 
 module.exports = (req, res, next) => {
   const { name, email, password, role } = req.body;
 
-  const { error } = schema.validate({ name, email, password, role });
+  const { error } = login.validate({ name, email, password, role });
 
-  if (error) {
-    error.status = 422;
-    return next(error);
-  }
+  if (error) return next(error);
 
   next();
 };
