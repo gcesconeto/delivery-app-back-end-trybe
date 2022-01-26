ccc5908a69d76bc6f4b04c03d3800adce1af0379
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-25 21:00:02 -0300

    Cria `service` de registro de usuário

--- back-end/src/errors/errors.js
+++ back-end/src/errors/errors.js
@@ -1,1 +1,3 @@
 module.exports.NOT_FOUND = { status: 404, message: 'nao achou' };
+
+module.exports.ALREADY_EXISTS = { status: 409, message: 'Usuário já registrado.' };
--- back-end/src/services/user/create.js
+++ back-end/src/services/user/create.js
@@ -1,5 +1,17 @@
-// const err = require('../../errors/errors');
+const md5 = require('md5');
 
-// module.exports = () => {
-//     throw err.NOT_FOUND;
-// };
+const { user } = require('../../database/models');
+const err = require('../../errors/errors');
+
+module.exports = async ({ name, email, password, role }) => {
+    if (await user.findOne({ where: { email } })) throw err.ALREADY_EXISTS;
+
+    const createdUser = await user.create({
+      name,
+      email,
+      password: md5(password),
+      role,
+    });
+
+    return createdUser;
+};
