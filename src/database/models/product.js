872f42b6e666654d29a80e251a1c51ddd6f00a8f
Guilherme Cesconeto <cesconeto@gmail.com>
2022-03-17 15:37:59 -0300

    docs: starting the readme.md

--- src/database/models/product.js
+++ src/database/models/product.js
@@ -1,26 +1,25 @@
 module.exports = (sequelize, DataTypes) => {
-    const product = sequelize.define('product',
-    {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      price: {
-        type: DataTypes.DECIMAL,
-        allowNull: false,
-        unique: true,
-      },
-      urlImage: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+  const product = sequelize.define('product',
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    {
-      tableName: 'products',
-      timestamps: false,
-      underscored: true,
-    });
-  
-    return product;
-  };
-  
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      unique: true,
+    },
+    urlImage: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'products',
+    timestamps: false,
+    underscored: true,
+  });
+
+  return product;
+};
--- src/database/models/sale.js
+++ src/database/models/sale.js
@@ -1,48 +1,47 @@
 module.exports = (sequelize, DataTypes) => {
-    const sale = sequelize.define('sale',
-    {
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        foreignKey: true
-      },
-      sellerId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        foreignKey: true
-      },
-      totalPrice: {
-        type: DataTypes.DECIMAL,
-        allowNull: false,
-      },
-      deliveryAddress: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      deliveryNumber: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      status: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+  const sale = sequelize.define('sale',
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      foreignKey: true
     },
-    {
-      tableName: 'sales',
-      timestamps: true,
-      createdAt: 'sale_date',
-      updatedAt: false,
-      underscored: true,
-    });
-  
-    sale.associate = (models) => {
-      sale.belongsTo(models.user,
-        { foreignKey: 'userId', as: 'user' })
-      sale.belongsTo(models.user,
-        { foreignKey: 'sellerId', as: 'seller' });
-    };
-  
-    return sale;
+    sellerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      foreignKey: true
+    },
+    totalPrice: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    deliveryAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    deliveryNumber: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'sales',
+    timestamps: true,
+    createdAt: 'sale_date',
+    updatedAt: false,
+    underscored: true,
+  });
+
+  sale.associate = (models) => {
+    sale.belongsTo(models.user,
+      { foreignKey: 'userId', as: 'user' })
+    sale.belongsTo(models.user,
+      { foreignKey: 'sellerId', as: 'seller' });
   };
-  
+
+  return sale;
+};
